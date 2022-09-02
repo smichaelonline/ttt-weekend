6404228707ff0881d029c41ccfb3fb2c3a2fb418
smichaelonline <stephaniemichael001@gmail.com>
2022-09-02 10:13:55 -0400

    create variables and cached element references. Create ininitialize function.

--- js/app.js
+++ js/app.js
@@ -4,15 +4,25 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board, turn, winner
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+let guessesEl = document.querySelector("div")
+let messageEl = document.querySelector('#message')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+init()
+
+function init(){
+  board = [null,null,null,null,null,null,null,null,null]
+  turn = 1 //represents player X
+  winner = null 
+  render()
+}
 
